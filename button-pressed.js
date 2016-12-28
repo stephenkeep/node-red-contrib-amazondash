a784a58a50107db1fbc431b76b4671b5fc09e142
Stuart Grimshaw <stuart.grimshaw@gmail.com>
2016-12-28 21:30:05 +0000

    Updated to new node-button version & improved logging.

--- button-pressed.js
+++ button-pressed.js
@@ -3,23 +3,27 @@ var dash_button = require('node-dash-button'),
 
 module.exports = function(RED) {
     function node (config) {
-        
+
         RED.nodes.createNode(this, config);
         var node = this;
-        
-        var mac = config.mac || '';
-        
-        console.log(mac);
 
-        var dash = dash_button(mac); 
+        var mac = config.mac || '';
+        var iface = config.iface || undefined;
+
+        node.log('Watching for button with mac address: ' + mac);
+
+        var dash = dash_button(mac, iface, 5000, 'all');
         var found = function () {
-            console.log('Button Pressed: ' + mac);
-            var msg = {};
+            node.log('Dash button pressed: ' + mac);
+            var msg = {
+                mac: config.mac,
+                name: config.name
+            };
             node.send(msg);
         };
-        
+
         dash.on("detected", _.debounce(found, 5000, true));
     };
- 
+
     RED.nodes.registerType("ButtonPressed",node);
 }
