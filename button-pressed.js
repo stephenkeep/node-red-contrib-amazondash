8af6e4a7c619da7d9ba3a2d7c9927a4fde8f5a7d
Thomas Nordquist <thomasnordquist@users.noreply.github.com>
2017-11-24 00:29:38 +0100

    Update scan syntax

--- button-pressed.js
+++ button-pressed.js
@@ -3,23 +3,20 @@ var dash_button = require('node-dash-button'),
 
 module.exports = function(RED) {
     function node (config) {
-        
         RED.nodes.createNode(this, config);
         var node = this;
-        
-        var mac = config.mac || '';
-        
-        console.log(mac);
 
-        var dash = dash_button(mac); 
+        var mac = config.mac || '';
+        var dash = dash_button(mac, null, null, 'all');
         var found = function () {
             console.log('Button Pressed: ' + mac);
-            var msg = {};
-            node.send(msg);
+            node.send({
+                payload: mac
+            });
         };
-        
+
         dash.on("detected", _.debounce(found, 5000, true));
     };
- 
+
     RED.nodes.registerType("ButtonPressed",node);
 }
